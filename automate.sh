e0833345323e91066c58b7b6d1070ebf162812df
Linden Burack <linden.burack@gmail.com>
2024-09-04 16:29:16 +0200

    Remove unwanted files

--- automate.sh
+++
@@ -1,7 +0,0 @@
-
-modes=(3 5 6 7 9 10 11 12 13 14 15)
-
-for i in ${modes[@]}; do
-    #Note the last job submitted
-    python3 BDT.py -m $i &
-done
--- lazy.sh
+++
@@ -1,6 +0,0 @@
-modes=(3 5 6 1 7 9 10 11 12 13 14 15)
-
-for i in ${modes[@]}; do
-	echo $i
-	mkdir ./${i}
-done
